76f10db06313d5a6c563aa6532f41e9dcf5d9f6a
Pravinkumar <pravinkumarsaminathan@gmail.com>
2024-07-28 13:59:30 +0530

    adding of logo for website

--- _templates/_head.php
+++ _templates/_head.php
@@ -1,6 +1,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="assests/side_logo.svg">
     <title>Search</title>
     <?php if (file_exists($_SERVER['DOCUMENT_ROOT']."/css/".basename($_SERVER['PHP_SELF'],".php").".css"))
     { ?>
