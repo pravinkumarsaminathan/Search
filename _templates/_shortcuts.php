6278005049d2cdb19122bfdcba69fbc2eaa155df
pravinkumarsaminathan <143002664+pravinkumarsaminathan@users.noreply.github.com>
2024-08-01 11:06:16 +0530

    Update _shortcuts.php

--- _templates/_shortcuts.php
+++ _templates/_shortcuts.php
@@ -1,37 +1,37 @@
 <div class="shortcuts">
-        <a href="<?php print($__sublink);?>/Search/result.php?search=cherry" class="shortcut">
+        <a href="https://fruits.selfmade.solutions/result.php?search=cherry" class="shortcut">
             <img src="assests/cherries.svg" alt="blockbox" width="24" height="24">
             <span>Cherry</span>
         </a>
-        <a href="<?php print($__sublink);?>/Search/result.php?search=apple" class="shortcut">
+        <a href="https://fruits.selfmade.solutions/result.php?search=apple" class="shortcut">
             <img src="assests/apple.svg" alt="gitlab.ninja">
             <span>Apple</span>
         </a>
-        <a href="<?php print($__sublink);?>/Search/result.php?search=pineapple" class="shortcut">
+        <a href="https://fruits.selfmade.solutions/result.php?search=pineapple" class="shortcut">
             <img src="assests/pineapple.svg" alt="DASHBOARD">
             <span>Pineapple</span>
         </a>
-        <a href="<?php print($__sublink);?>/Search/result.php?search=banana" class="shortcut">
+        <a href="https://fruits.selfmade.solutions/result.php?search=banana" class="shortcut">
             <img src="assests/bananas.svg" alt="GPT">
             <span>Banana</span>
         </a>
-        <a href="<?php print($__sublink);?>/Search/result.php?search=orange" class="shortcut">
+        <a href="https://fruits.selfmade.solutions/result.php?search=orange" class="shortcut">
             <img src="assests/orange.svg" alt="DevDocs">
             <span>Orange</span>
         </a>
-        <a href="<?php print($__sublink);?>/Search/result.php?search=blueberry" class="shortcut">
+        <a href="https://fruits.selfmade.solutions/result.php?search=blueberry" class="shortcut">
             <img src="assests/blueberry.svg" alt="skillrack">
             <span>blueberry</span>
         </a>
-        <a href="<?php print($__sublink);?>/Search/result.php?search=grape" class="shortcut">
+        <a href="https://fruits.selfmade.solutions/result.php?search=grape" class="shortcut">
             <img src="assests/grape.svg" alt="YOUTUBE">
             <span>Grape</span>
         </a>
-        <a href="<?php print($__sublink);?>/Search/result.php?search=lychee" class="shortcut">
+        <a href="https://fruits.selfmade.solutions/result.php?search=lychee" class="shortcut">
             <img src="assests/lychee.svg" alt="w3">
             <span>Lychee</span>
         </a>
-        <a href="<?php print($__sublink);?>/Search/result.php?search=watermelon" class="shortcut">
+        <a href="https://fruits.selfmade.solutions/result.php?search=watermelon" class="shortcut">
             <img src="assests/watermelon.svg" alt="Bootstrap">
             <span>Watermelon</span>
         </a>
