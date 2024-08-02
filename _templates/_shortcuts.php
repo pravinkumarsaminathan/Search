262453ac57219f88cee7230ad43a5ed6169e497a
pravinkumarsaminathan <143002664+pravinkumarsaminathan@users.noreply.github.com>
2024-08-02 13:18:17 +0530

    Update _shortcuts.php

--- _templates/_shortcuts.php
+++ _templates/_shortcuts.php
@@ -1,38 +1,38 @@
 <div class="shortcuts">
         <a href="https://fruits.selfmade.solutions/result.php?search=cherry" class="shortcut">
-            <img src="assests/cherries.svg" alt="blockbox" width="24" height="24">
+            <img src="assests/cherries.svg" alt="Cherry" width="24" height="24">
             <span>Cherry</span>
         </a>
         <a href="https://fruits.selfmade.solutions/result.php?search=apple" class="shortcut">
-            <img src="assests/apple.svg" alt="gitlab.ninja">
+            <img src="assests/apple.svg" alt="Apple">
             <span>Apple</span>
         </a>
         <a href="https://fruits.selfmade.solutions/result.php?search=pineapple" class="shortcut">
-            <img src="assests/pineapple.svg" alt="DASHBOARD">
+            <img src="assests/pineapple.svg" alt="Pineapple">
             <span>Pineapple</span>
         </a>
         <a href="https://fruits.selfmade.solutions/result.php?search=banana" class="shortcut">
-            <img src="assests/bananas.svg" alt="GPT">
+            <img src="assests/bananas.svg" alt="Banana">
             <span>Banana</span>
         </a>
         <a href="https://fruits.selfmade.solutions/result.php?search=orange" class="shortcut">
-            <img src="assests/orange.svg" alt="DevDocs">
+            <img src="assests/orange.svg" alt="Orange">
             <span>Orange</span>
         </a>
         <a href="https://fruits.selfmade.solutions/result.php?search=blueberry" class="shortcut">
-            <img src="assests/blueberry.svg" alt="skillrack">
+            <img src="assests/blueberry.svg" alt="Blueberry">
             <span>blueberry</span>
         </a>
         <a href="https://fruits.selfmade.solutions/result.php?search=grape" class="shortcut">
-            <img src="assests/grape.svg" alt="YOUTUBE">
+            <img src="assests/grape.svg" alt="Grape">
             <span>Grape</span>
         </a>
         <a href="https://fruits.selfmade.solutions/result.php?search=lychee" class="shortcut">
-            <img src="assests/lychee.svg" alt="w3">
+            <img src="assests/lychee.svg" alt="Lychee">
             <span>Lychee</span>
         </a>
         <a href="https://fruits.selfmade.solutions/result.php?search=watermelon" class="shortcut">
-            <img src="assests/watermelon.svg" alt="Bootstrap">
+            <img src="assests/watermelon.svg" alt="Watermelon">
             <span>Watermelon</span>
         </a>
     </div>
